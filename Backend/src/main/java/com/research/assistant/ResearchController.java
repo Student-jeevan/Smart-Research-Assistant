b843d79f5ea396f9d53323fe4c98131ad6d9dc99
jeevansingh <jeevanthitrya@gmail.com>
2025-10-16 23:18:49 +0530

    Add Backend and Chrome-Extension as normal folders

---
+++ Backend/src/main/java/com/research/assistant/ResearchController.java
@@ -0,0 +1,19 @@
+package com.research.assistant;
+
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/research")
+@CrossOrigin(origins = "*")
+@AllArgsConstructor
+public class ResearchController {
+    private ResearchService researchService;
+
+    @PostMapping("/process")
+    public ResponseEntity<String> processContent(@RequestBody ResearchRequest request){
+        String res = researchService.processContent(request);
+        return ResponseEntity.ok(res);
+    }
+}
